f44e86425735187e5e67021f74329c98ea3bdbce
shainaz1207 <105064443+shainaz1207@users.noreply.github.com>
2022-05-31 21:48:15 +0530

    Create Inheritence.py

---
+++ Inheritence.py
@@ -0,0 +1,66 @@
+"""parent class"""
+# class Animal:
+#     Livingthing=True
+
+#     def eat():
+#         print("eating")
+
+#     def sleep():
+#         print("sleep")
+
+# # """child class"""
+# class goat(Animal):
+
+#     def grass():
+#         print("Its eating grass")
+
+#     def herb():
+#         print("It is herbivorus")
+
+#     def sleep():
+#         print("Don't sleep you are 820Rs kg")
+
+# print(goat.livingthing)  # child class and acessing the parent class variable
+# print(Animal.livingthing)  # parent class and acessing classvariable
+# goat.grass()
+# goat.herb()
+# goat.eat()
+# goat.sleep()
+# Animal.eat()
+# Animal.sleep()
+# Animal.herb()
+
+""" part-2 calling with thehelp of an object"""
+# class Animal:
+"     livingthing=true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
